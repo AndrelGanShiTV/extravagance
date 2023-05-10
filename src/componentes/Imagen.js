c96d1e9c3423250d668e602c9c9d16fdeb3ba702
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-05-10 10:44:20 -0500

    Instalacion de paquetes de react y componente img

---
+++ src/componentes/Imagen.js
@@ -0,0 +1,12 @@
+function Imagen() {
+    const imagenDefault = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBR18fTKmQmFh9ZnCw2nTPqjC2KcgdFysK44oozmo&s';
+    return (
+        <>
+            <div>
+                <img src={imagenDefault} />
+            </div>
+        </>
+    );
+} 
+
+export default Imagen
