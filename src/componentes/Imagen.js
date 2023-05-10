0f9b936c8a2aee16959474b7b36c4f9cb3f31847
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-05-10 11:04:10 -0500

    Img cosas

--- src/componentes/Imagen.js
+++ src/componentes/Imagen.js
@@ -1,5 +1,6 @@
 function Imagen() {
     const imagenDefault = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBR18fTKmQmFh9ZnCw2nTPqjC2KcgdFysK44oozmo&s';
+
     return (
         <>
             <div>
