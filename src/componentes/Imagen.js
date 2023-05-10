1e4dbbb428d7728a118c55f457b16f11059223b5
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-05-10 17:56:49 -0500

    Imagen Volver

--- src/componentes/Imagen.js
+++ src/componentes/Imagen.js
@@ -1,8 +1,10 @@
-function Imagen(props) {
+function Imagen() {
+    const imagenDefault = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBR18fTKmQmFh9ZnCw2nTPqjC2KcgdFysK44oozmo&s';
+
     return (
         <>
             <div>
-                <img src={props.image} />
+                <img src={imagenDefault} />
             </div>
         </>
     );
