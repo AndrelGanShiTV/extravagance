d58f2182aaa7c997b8c038ccb83af09fdfc9ff82
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-05-14 21:41:35 -0500

    Nuevos Componentes y Rutas

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import Nav from './componentes/Nav/Nav'
 import Carrusel from './componentes/ImagenesCarrusel/Carrusel'
 import Ingresar from './componentes/Ingresar/Ingresar'
+import Error from './componentes/Error/Error'
+import Home from './componentes/Home/Home'
 
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Nav />
-        <Carrusel />
-         <Ingresar/>
-      </div>
+      <nav>
+      <Router>
+        <Nav/>
+        <Routes>
+              <Route exact path='/' element={<Home />}></Route> {/* exact pone por defecto el componente indicado en element*/}
+              <Route path='*' element={<Error />}></Route> {/* Si no encuentra la página se dirige al componente Error*/}
+              <Route path='/Ingresar' element={<Ingresar />}></Route>
+        </Routes>
+      </Router>
+      </nav>
     </>
   );
 }
---
+++ src/componentes/Error/Error.js
@@ -0,0 +1,10 @@
+function Error(){
+    return(
+        <div>
+            <h1>ERROR 404</h1>
+            <h2>La página no fue encontrada</h2>
+        </div>
+    );
+}
+
+export default Error
